bf1c02a2082716f6a7412a744be5714d9b5a9af9
Kenuit <kenuit2004@gmail.com>
2026-09-22 13:32:34 +0530

    Basic ArrayList Program

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Main 
+{
+	public static void main(String[] args) 
+	{
+		List<String> a=new ArrayList<>();
+		a.add("Lion");
+		a.add("Tiger");
+		a.add("Dog");
+		a.add("Cat");
+		for(String b:a)
+		{
+			System.out.println(b);
+		}
+	}
+}
